bd5f7eae02977daef25b31dacc1e82fa9fbe8f42
Riley Rager <276484@gcloud.tisd.org>
2025-01-13 19:34:04 +0000

    upload

---
+++ studentportfoilo/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Header from "./parts/Header/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header></Header>
+    </>
+
+  )
+}
---
+++ studentportfoilo/src/app/parts/Header/Header.tsx
@@ -0,0 +1,15 @@
+import "./css/index.css"
+
+
+export default function Header() {
+    return (
+        <header className="Header">
+            <h1 className="HeaderText">
+                My portfilo
+                <span className="first-line "> Temple</span>
+                <span className="second-line"> Highschool </span>
+            </h1>
+            <p>Hello, I'm Riley Rager, a high school student</p>
+        </header>
+    )
+}
